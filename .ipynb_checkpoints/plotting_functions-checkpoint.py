ebf5a125b94b203719706635867e482f73483ad3
khsjursen <kasj@hvl.no>
2023-07-12 22:14:39 +0200

    added function plot_feature_permutation_importance

---
+++ .ipynb_checkpoints/plotting_functions-checkpoint.py
@@ -0,0 +1,139 @@
+# Import libraries
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import KFold
+from sklearn.model_selection import LeaveOneGroupOut
+from sklearn.inspection import permutation_importance
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plot_prediction(y1, y2, data_type:str, n_toplot=10**10):
+    """
+    Plot model predictions y1 vs. actual observations y2 and show
+    calculated error metrics.
+
+    Parameters:
+    y1 : np.array
+        Predicted labels.
+    y2 : np.array
+        Actual labels.
+    data_type : str
+        Type of data, e.g. "Validation" or "Test".
+    n_toplot : int
+        Number of points to plot. 
+    """
+    
+    from scipy.stats import gaussian_kde
+    from sklearn.metrics import r2_score, mean_squared_error
+    
+    idxs = np.arange(len(y1))
+    np.random.shuffle(idxs)
+
+    y_max = 7 #max(max(y1), max(y2))[0] + 1
+    y_min = -1 #min(min(y1), min(y2))[0] - 1
+    
+    y_expected = y1.reshape(-1)[idxs[:n_toplot]]
+    y_predicted = y2.reshape(-1)[idxs[:n_toplot]]
+
+    xy = np.vstack([y_expected, y_predicted])
+    z = gaussian_kde(xy)(xy)
+    # Sort the points by density, so that the densest points are plotted last
+    idx = z.argsort()
+    y_plt, ann_plt, z = y_expected[idx], y_predicted[idx], z[idx]
+    
+    plt.figure(figsize=(8,8))
+    plt.title("Model Evaluation " + data_type, fontsize=17)
+    plt.ylabel('Modeled SMB (m.w.e)', fontsize=16)
+    plt.xlabel('Reference SMB (m.w.e)', fontsize=16)
+    sc = plt.scatter(y_plt, ann_plt, c=z, s=20)
+    plt.clim(0,0.4)
+    plt.tick_params(labelsize=14)
+    plt.colorbar(sc) 
+    lineStart = y_min
+    lineEnd = y_max
+    plt.plot([lineStart, lineEnd], [lineStart, lineEnd], 'k-')
+    plt.axvline(0.0, ls='-.', c='k')
+    plt.axhline(0.0, ls='-.', c='k')
+    plt.xlim(lineStart, lineEnd)
+    plt.ylim(lineStart, lineEnd)
+    plt.gca().set_box_aspect(1)
+    
+    textstr = '\n'.join((
+    r'$MSE=%.2f$' % (mean_squared_error(y_expected, y_predicted, squared=True), ),
+    r'$R^2=%.2f$' % (r2_score(y_expected, y_predicted), )))
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    # place a text box in upper left in axes coords
+    plt.text(0.05, 0.95, textstr, transform=plt.gca().transAxes, fontsize=14,
+            verticalalignment='top', bbox=props)
+    
+    plt.show()
+
+def plot_prediction_per_fold(X, y, model, idc_list):
+    """
+    Plot model predictions of model vs. test data y based on 
+    folds given by indices X_idc and y_idc.
+  
+    Parameters:
+    X : np.array
+        Training dataset of features.
+    y : np.array
+        Labels of training dataset.
+    model : sklearn.model
+        Fitted XGBmodel object.
+    X_idc : np.array
+        Indices of fold splits of features.
+    y_idc : np.array
+        Indices of fold splits for labels.
+    """
+
+    y_test_list = []
+    y_pred_list = []
+
+    for train_index, test_index in idc_list:
+        # Loops over n_splits iterations and gets train and test splits in each fold
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+
+        y_test_list.extend(y_test)
+        y_pred_list.extend(y_pred)
+
+    # Arrays of predictions and observations for each fold
+    y_test_all = np.hstack([*y_test_list])
+    y_pred_all = np.hstack([*y_pred_list])
+
+    # Plot predictions and observations for each cross-valiadation fold together
+    plot_prediction(y_test_all, y_pred_all, 'Validation', n_toplot=5000)
+
+#def plot_feature_importance(
+
+
+def plot_feature_permutation_importance(model, X, y, X_labels):
+
+    model.fit(X, y)
+    feature_importance = model.feature_importances_
+    sorted_idx = np.argsort(feature_importance)
+    pos = np.arange(sorted_idx.shape[0]) + 0.5
+    fig = plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.barh(pos, feature_importance[sorted_idx], align="center")
+    plt.yticks(pos, X_labels[sorted_idx])
+    plt.title("Feature Importance (MDI)")
+
+    result = permutation_importance(
+        model, X, y, n_repeats=10, random_state=42, n_jobs=2
+    )
+    sorted_idx = result.importances_mean.argsort()
+    plt.subplot(1, 2, 2)
+    plt.boxplot(
+        result.importances[sorted_idx].T,
+        vert=False,
+        labels=X_labels[sorted_idx],
+    )
+    plt.title("Permutation Importance (test set)")
+    fig.tight_layout()
+    plt.show()
+
